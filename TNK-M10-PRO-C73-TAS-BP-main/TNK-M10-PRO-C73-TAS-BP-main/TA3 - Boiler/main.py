1f53d1a1e3fb777631ca530a7ace15b7db5095fc
Kilativ1005 <158013302+Kilativ1005@users.noreply.github.com>
2024-02-26 16:36:05 +1100

    Add files via upload

---
+++ TNK-M10-PRO-C73-TAS-BP-main/TNK-M10-PRO-C73-TAS-BP-main/TA3 - Boiler/main.py
@@ -0,0 +1,42 @@
+import cv2  
+import numpy as np
+from keras.models import load_model  
+
+model = load_model("keras_Model.h5", compile=False)
+
+classNames = open("labels.txt", "r").readlines()
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    ret, image = camera.read()
+
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Print the class name, confidence score to screen output
+    try:
+        image = cv2.putText(image, classNames[2:], (10, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0), 3)
+        image = cv2.putText(image, str(np.round(confidenceScore*100)) [:-2], (15, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 3 )
+    except:
+        pass
+    cv2.imshow("Webcam Image", image)
+ 
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    image = (image / 127.5) -1
+
+    # Use model to pridict the output for current image
+    prediction = model.predict(image)
+    # Use np.argmax to find a index with highest value in prediction array
+    index = np.argmax(prediction)
+    # Use the index to get the name from classNames
+    classNames = classNames[index]
+    # Get confidenceScore from prediction[0] at index 
+    confidenceScore = prediction[0][index]
+
+    keyboardInput = cv2.waitKey(1)
+    if keyboardInput == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
